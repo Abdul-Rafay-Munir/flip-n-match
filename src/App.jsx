df5a00331eab2225996f3d89f56beb6ec10b7a98
Abdul-Rafay-Munir <munirabdulrafay@gmail.com>
2026-01-03 16:10:17 +0500

    created cards

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,34 @@
+import { Card } from "./components/Card";
 import { GameHeader } from "./components/GameHeader";
+
+const cardValues = [
+  "🍎",
+  "🍊",
+  "🍋",
+  "🍌",
+  "🍉",
+  "🍇",
+  "🍓",
+  "🍒",
+  "🍎",
+  "🍊",
+  "🍋",
+  "🍌",
+  "🍉",
+  "🍇",
+  "🍓",
+  "🍒",
+];
 
 function App() {
   return (
     <div className="app">
-      <GameHeader />
+      <GameHeader score={0} moves={0} />
+      <div className="cards-grid">
+        {cardValues.map((card) => (
+          <Card card={card} />
+        ))}
+      </div>
     </div>
   );
 }
